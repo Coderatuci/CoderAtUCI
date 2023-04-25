0515053498249ff90d9e416c88b728a17f7bfe64
Coderatuci <131698379+Coderatuci@users.noreply.github.com>
2023-04-24 18:41:39 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Bio of Nika Niksefat</title>
+  </head>
+  <body>
+    <h1>Introduction</h1>
+    <h4> This is Nika Niksefat a coder at UCI</h4>
+  </body>
+</html>
 
