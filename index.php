5842fb7fc6ba50125e9b3da5fb254eef73f16ef6
Coderatuci <131698379+Coderatuci@users.noreply.github.com>
2023-05-01 22:03:16 -0700

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <title>Bio of Nika Niksefat</title>
   </head>
   <body>
-    <a href="css-demo.html"> This is a link to the demo file </a>
+    <a href="css_demo.html"> This is a link to the demo file </a>
     <h1>Introduction</h1>
     <h4> This is Nika Niksefat a coder at UCI</h4>
   </body>
