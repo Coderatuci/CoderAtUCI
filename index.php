82dd8c239968180c2554fd135eba1625e8153a1d
Coderatuci <131698379+Coderatuci@users.noreply.github.com>
2023-05-05 13:49:58 -0700

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
   <body>
     <a href="css_demo.html"> This is a link to the demo file </a>
     <p><a href="font-sample.html"> This is a link to the font-sample file</a></p>
-    <p><a bref="bootstrap-sample.html"> This is a link to the bootstrap-sample file</a></p>
+    <p><a href="bootstrap-sample.html"> This is a link to the bootstrap-sample file</a></p>
     <h1>Introduction</h1>
     <h4> This is Nika Niksefat a coder at UCI</h4>
   </body>
