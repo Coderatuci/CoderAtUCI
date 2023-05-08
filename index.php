3271a6fc5de2595fae1a7c5fb9ddefbd3dc78a52
Coderatuci <131698379+Coderatuci@users.noreply.github.com>
2023-05-07 22:03:38 -0700

    Update index.php

--- index.php
+++ index.php
@@ -5,11 +5,12 @@
   </head>
   <body>
     <a href="css_demo.html"> This is a link to the demo file </a>
+    <p>This is a link to link Guthub to Heroku</p>
     <p><a href="font-sample.html"> This is a link to the font-sample file</a></p>
     <p><a href="bootstrap-sample.html"> This is a link to the bootstrap-sample file</a></p>
     <h1>Introduction</h1>
     <h4> This is Nika Niksefat a coder at UCI</h4>
   </body>
 </html>
-<p>I'm going to clean up my index.html file</p>
+
 
