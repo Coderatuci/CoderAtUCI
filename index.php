546b39b286af7f0f39590712e0de8df46f8235d7
DESKTOP-6ON2OLI\Nika <niksefan@uci.edu>
2023-05-07 23:45:48 -0700

    Deleted the pipeline

--- index.php
+++ index.php
@@ -1,16 +1,4 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Bio of Nika Niksefat</title>
-  </head>
-  <body>
-    <a href="css_demo.html"> This is a link to the demo file </a>
-    <p>This is a link to link Guthub to Heroku</p>
-    <p><a href="font-sample.html"> This is a link to the font-sample file</a></p>
-    <p><a href="bootstrap-sample.html"> This is a link to the bootstrap-sample file</a></p>
-    <h1>Introduction</h1>
-    <h4> This is Nika Niksefat a coder at UCI</h4>
-  </body>
-</html>
+<?php header ('Location:/index.html');?>
+
 
 
